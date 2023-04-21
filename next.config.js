88baaf4d17b0bbbc98391cef4d19a77cc012f81a
jsalamanc <jsalamancacuello@gmail.com>
2023-04-21 12:20:25 -0500

    configurando el nextconfig

--- next.config.js
+++ next.config.js
@@ -1,8 +1,39 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+];
+
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_URL_PAGE: 'http://localhost:3000'
+  images: {
+    domains: [
+      'imgix.cosmicjs.com',
+    ],
+    formats: ['image/avif', 'image/webp'],
+  },
+  webpack: (config) => {
+    config.optimization.splitChunks.cacheGroups = {};
+    config.optimization.minimize = true;
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
   }
 };
 
